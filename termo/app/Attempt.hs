441a9fb335b58efe98e46ff8af9759674e7f0457
PabloChristianno <pablochristianno@gmail.com>
2024-11-07 15:36:19 -0300

    feat: Verifica se uma palavra esta no banco de palavras

--- termo/app/Attempt.hs
+++ termo/app/Attempt.hs
@@ -1,5 +1,10 @@
-module Attempt where
+module Attempt
+    (
+        getAttempt,
+        isValidAttempt
+    ) where
 
+import System.IO(openFile, hClose, hIsEOF, hGetLine, Handle, IOMode(..))
 
 -- TODO: Issue 5
 showAttemptNum :: Int -> IO ()
@@ -14,6 +19,24 @@ getAttempt = do
     line <- getLine
     return line
 
+-- Verifica se a palavra de entrada existe no banco de palavras
+isValidAttempt :: String -> IO Bool
+isValidAttempt sword = do
+    file <- openFile "Words.txt" ReadMode
+    result <- readLines file
+    hClose file
+    return result
+ where
+    readLines :: Handle -> IO Bool
+    readLines file = do
+        eof <- hIsEOF file
+        if eof
+            then return False
+        else do    
+            line <- hGetLine file
+            if elem sword (words line)
+                then return True
+            else readLines file
 
 -- TODO: Issue 9
 -- Aqui talvez seja melhor passar o data inteiro de gameState e retornar ele modificado (Relacioando com a Issue 2)

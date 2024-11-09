c3b1bd370d10fe10d28fd82247355c726e991acc
fellipepcs <fellipy.02@hotmail.com>
2024-11-09 00:07:52 -0300

    feat: adicionado app game com integracao ao letter status

--- termo/app/Game.hs
+++ termo/app/Game.hs
@@ -1,43 +1,51 @@
 module Game where
 
-import Attempt (showAttemptNum, getAttempt, processAttempt)
+import LetterStatus (LetterStatus(..))
 import Words (getSecretWord)
-import Menu (showScore)
+import System.Console.ANSI (setSGR, SGR(SetColor, Reset), ColorIntensity(Vivid), Color(..), ConsoleLayer(Foreground))
 
+statusToColor :: LetterStatus -> Color
+statusToColor NoExist = Red
+statusToColor RightPlace = Green
+statusToColor WrongPlace = Yellow
+statusToColor _ = White 
+
+printStyledLetter :: (Char, LetterStatus) -> IO ()
+printStyledLetter (ch, status) = do
+    setSGR [SetColor Foreground Vivid (statusToColor status)]
+    putStr [ch]
+    setSGR [Reset]
+
+processAndPrintAttempt :: String -> String -> IO ()
+processAndPrintAttempt secretWord attempt = do
+    let statuses = zipWith (compareLetters secretWord) secretWord attempt
+    mapM_ printStyledLetter statuses
+    putStrLn ""
+
+compareLetters :: String -> Char -> Char -> (Char, LetterStatus)
+compareLetters secretWord secretLetter attemptLetter
+    | attemptLetter == secretLetter = (attemptLetter, RightPlace)
+    | attemptLetter `elem` secretWord = (attemptLetter, WrongPlace)
+    | otherwise = (attemptLetter, NoExist)
 
 runGame :: IO ()
 runGame = do
-    -- Variáveis globais do jogo
-    -- TODO: Issue 2
     secretWord <- getSecretWord
     let maxAttempts = 6
-
-    -- Loop do jogo
     loopGame secretWord maxAttempts
-
-
 
 loopGame :: String -> Int -> IO ()
 loopGame secretWord attemptNum = do
-    -- Limpando tela
-    -- putStrLn "\ESC[2J"
+    putStrLn ("Tentativa " ++ show attemptNum ++ ":")
+    attempt <- getLine
+    processAndPrintAttempt secretWord attempt
 
-    -- Exibindo tentativas restantes na tela
-    showAttemptNum attemptNum
-
-    -- Recebendo input do jogador
-    attempt <- getAttempt
-
-    -- Processando tentativa
-    rightAns <- processAttempt secretWord attempt
-
-    -- Verificando se o jogo acabou
-    if rightAns then do
-        showScore secretWord attemptNum
+    if attempt == secretWord then do
+        putStrLn "\nVocê acertou! Parabéns!"
         return ()
     else do
         let newAttemptNum = attemptNum - 1
         if newAttemptNum <= 0 then do
-            showScore secretWord attemptNum
+            putStrLn "\nVocê não acertou. Fim de jogo!"
             return ()
         else loopGame secretWord newAttemptNum
